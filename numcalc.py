4f9df9e261c9db76c7a9a9d6edb047aaa500d03b
Victor <v_m_1732@orange.fr>
2020-04-11 15:35:32 +0200

    Added a bunch of things

--- numcalc.py
+++ numcalc.py
@@ -2,6 +2,7 @@ import os
 import time
 import shutil
 import random
+import math
 
 import keyboard
 import termfmt as tfmt
@@ -48,8 +49,32 @@ def genProd(a, b, shuf=False):
     if shuf: a,b=b,a
     return Question(str(a)+"*"+str(b)+"=", str(a*b))
 
+def genFrac(a,b):
+    return Question(str(a)+"/"+str(b)+"=", str(a//math.gcd(a,b))+"/"+str(b//math.gcd(a,b)))
+
+def genDecomp(n):
+    def decomp(n):
+        ret=[]
+        d=2
+        while n!=1:
+            if n%d: d+=1
+            else:
+                n//=d
+                ret.append(d)
+        return ret
+
+    return Question(str(n)+"=", "*".join(map(str, decomp(n))))
+
+
 def genListMinMax(func, nMin, nMax, nb):
     return [func(random.randint(nMin, nMax), random.randint(nMin, nMax)) for k in range(nb)]
+
+def genListFrac(nMin, nMax, nb):
+    c=random.randint(nMin, nMax)
+    return [genFrac(random.randint(nMin, nMax)*c, random.randint(nMin, nMax)*c) for k in range(nb)]
+
+def genListDecomp(nMin, nMax, nb):
+    return [genDecomp(random.randint(nMin, nMax)) for k in range(nb)]
 
 def genTable(a):
     t=[genProd(a, k, shuf=True) for k in range(1,10)]
@@ -71,10 +96,10 @@ class ModeTest:
         elif '0' <= c <= '9':
             if self.pos < len(self.qList):
                 self.lastTime=time.time()
-                self.total+=1
                 ret=self.qList[self.pos].input(c)
                 self.missed+=ret
                 if self.qList[self.pos].full():
+                    self.total+=1
                     clearLine()
                     print(self.qList[self.pos].getText())
                     self.pos+=1
@@ -96,13 +121,13 @@ class ModeTest:
             rank=tfmt.fmt("S", tfmt.fgClorRGB(0, 255, 255)+tfmt.bold)
         elif perc>0.95:
             rank=tfmt.fmt("A", tfmt.fgClorRGB(0, 255, 0)+tfmt.bold)
-        elif perc>0.90:
+        elif perc>0.85:
             rank=tfmt.fmt("B", tfmt.fgClorRGB(128, 255, 0)+tfmt.bold)
-        elif perc>0.80:
-            rank=tfmt.fmt("C", tfmt.fgClorRGB(255, 255, 0)+tfmt.bold)
         elif perc>0.75:
-            rank=tfmt.fmt("D", tfmt.fgClorRGB(255, 128, 0)+tfmt.bold)
+            rank=tfmt.fmt("C", tfmt.fgClorRGB(255, 255, 0)+tfmt.bold)
         elif perc>0.50:
+            rank=tfmt.fmt("D", tfmt.fgClorRGB(255, 128, 0)+tfmt.bold)
+        elif perc>0.00:
             rank=tfmt.fmt("E", tfmt.fgClorRGB(255, 64, 0)+tfmt.bold)
 
         time=self.lastTime-self.startTime
@@ -119,8 +144,10 @@ class ModeTest:
 
 kb=keyboard.KBHit()
 
-mode=ModeTest(genListMinMax(genAdd, 10, 99, 20))
+#mode=ModeTest(genListMinMax(genProd, 10, 99, 20))
 #mode=ModeTest(genTable(3))
+#mode=ModeTest(genListFrac(10, 20, 10))
+mode=ModeTest(genListDecomp(10, 50, 10))
 newMode=None
 
 lastSize=os.terminal_size([0,0])
